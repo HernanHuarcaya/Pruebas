a6524dfcb9c1cf00f7e6ef83d9558eac4254dc11
Hernan Huarcaya <hernan.huarcaya@gmail.com>
2013-09-05 14:58:21 -0500

    Primer Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Tituo</title>
+        <style type="text/css">
+            body{
+                background: #ddd;
+            }
+            
+            form{
+                border: 1 solid darkred;
+                width: 250px;
+                height: 450px;
+            }
+        </style>
+    </head>
+    <body>
+        <form >
+            <input type="text" name="txtnombre"/><br>
+            <input type="checkbox" name="chkprueba" value="1" > Dato<br>
+            <textarea name="txtcomentario"></textarea>
+        </form>
+    </body>
+</html>
